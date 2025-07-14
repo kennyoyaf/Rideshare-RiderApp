3c74cd0f264966bae88d58a339a16d1d7a1bac28
kennyoyaf <kehindeoyafemi@gmail.com>
2025-07-14 14:48:54 +0100

    I have created the layout of the auth screen

---
+++ app/(auth)/_layout.tsx
@@ -0,0 +1,26 @@
+import { Stack } from "expo-router";
+import React from "react";
+
+const ScreenLayout: React.FC = () => {
+  return (
+    <>
+      <Stack>
+        <Stack.Screen name="enableLocation" options={{ headerShown: false }} />
+        <Stack.Screen name="setNewPassword" options={{ headerShown: false }} />
+        <Stack.Screen name="SignIn/sign-in" options={{ headerShown: false }} />
+        <Stack.Screen name="SignUp/sign-up" options={{ headerShown: false }} />
+        <Stack.Screen
+          name="SignUp/otpVerify"
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="SignUp/setPassword"
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen name="SignUp/profile" options={{ headerShown: false }} />
+      </Stack>
+    </>
+  );
+};
+
+export default ScreenLayout;
